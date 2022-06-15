93184ced11963fc09d9459fcef9f1c1b3612230a
YuliangWang316 <60790093+YuliangWang316@users.noreply.github.com>
2022-06-15 21:28:58 +0800

    Update gctcounts.R

--- gctcounts.R
+++ gctcounts.R
@@ -24,5 +24,10 @@ for (i in 1:length(a)) {
   colnames(b)<-colnames(e)
   f<-rbind(b,e)
   remove(b,e)
+  for (b in 1:length(rownames(f))) {
+    for (c in 1:length(colnames(f))) {
+      f[b,c]<-gsub('["]', '', f[b,c])
+    }
+  }
   write.table(f,paste0("D:/yyy/Merge_HUADA_YK_HUADA2/3vs3/gct1/",a[i],"_2.gct"),sep = "\t",row.names = FALSE,col.names = FALSE)
 }
