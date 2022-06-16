089e8ce2189b9ca6d83568c925bf8eaacd02f278
YuliangWang316 <60790093+YuliangWang316@users.noreply.github.com>
2022-06-16 22:42:48 +0800

    Add files via upload

---
+++ GSEA2.sh
@@ -0,0 +1,5 @@
+data_dir=/mnt/d/yyy/Merge_HUADA_YK_HUADA2/3vs3/gct2
+for file_a in ${data_dir}/*;do
+bash /mnt/d/GSEA_4.1.0/gsea-cli.sh GSEA -res $file_a -cls /mnt/d/yyy/sample3_vs_3.cls#KO_versus_WT -gmx  /mnt/d/h.all.v7.1.symbols.gmt  -collapse false -nperm 1000 -permute gene_set -rnd_type no_balance -scoring_scheme weighted -set_max 5000 -set_min 0 -rpt_label file -metric Signal2Noise -sort real -order descending -out /mnt/d/yyy/Merge_HUADA_YK_HUADA2/3vs3/result2
+done
+
